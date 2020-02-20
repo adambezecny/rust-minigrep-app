9451cd308360b23919e2e7a25ef7504bebcb3c0a
Adam Bezecny <adam.bezecny@gmail.com>
2020-02-20 22:33:01 +0100

    minor improvements

--- src/lib.rs
+++ src/lib.rs
@@ -11,14 +11,21 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next(); // first arg is the name of the binary being executed!
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
         Ok(Config { query, filename, case_sensitive })
     }
 }
@@ -40,7 +47,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
+    /*let mut results = Vec::new();
 
     for line in contents.lines() {
         if line.contains(query) {
@@ -48,7 +55,11 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
         }
     }
 
-    results
+    results */
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
+
 }
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
